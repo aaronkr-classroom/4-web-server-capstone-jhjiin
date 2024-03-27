a9e73b6ff039149098f3551ff24f14548012619a
2026074 조형진 <163215389+jhjiin@users.noreply.github.com>
2024-03-27 02:58:50 +0000

    수정 필요

---
+++ content-types.js
@@ -0,0 +1,24 @@
+// content-type.js
+"use strict";
+
+// listing 7.4 (p.112)
+module.exports = {
+    html: {
+        "Content-Type": "text/html"
+    },
+    text: {
+        "Content-Type": "text/plain"
+    },
+    css: {
+        "Content-Type": "text/css"
+    },
+    js: {
+        "Content-Type": "text/javascript"
+    },
+    jpg: {
+        "Content-Type": "text/jpg"
+    },
+    png: {
+    "Content-Type": "text/png"     
+    }
+}
---
+++ main.js
@@ -0,0 +1,58 @@
+"use strict";
+
+//필요 모듈 임포트
+const port = 3000,
+    http = require('http'),
+    httpStatus = require('http-status-codes'),
+    router = require('./router'),
+    contentType = require('./content-types'),
+    utils = require('./utils');
+//Listing 7.8 (p. 127-128)
+// GET 라우트
+router.get("/", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/index.html", res);
+});
+router.get("/courses", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/courses.html", res);
+});
+router.get("/contact", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/contact.html", res);
+});
+
+// POST 라우트
+router.post("/contact", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/thanks.html", res);
+});
+
+// 에셋 라우트
+// CSS 에셋 라우트
+router.get("/bootstrap.css", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.css);
+    utils.getFile("views/public/css/bootstrap.css", res);
+});
+router.get("/style.css", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.css);
+    utils.getFile("public/css/style.css", res);
+});
+// JS 에셋 라우트
+// IMG 에셋 라우트
+router.get("/people.jpg", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.jpg);
+    utils.getFile("public/img/people.jpg", res);
+});
+router.get("/product.jpg", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.jpg);
+    utils.getFile("public/img/product.jpg", res);
+});
+router.get("/graph.jpg", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.jpg);
+    utils.getFile("public/img/graph.jpg", res);
+});
+
+
+http.createServer(router.handle).listen(port);
+console.log(`Server at: http://localhost:${port}`);
